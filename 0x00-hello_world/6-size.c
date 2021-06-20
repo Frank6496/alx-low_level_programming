890a0c5f5b7854617f051a9e038896d2586ef4b5
Frank6496 <francismuthoni254@gmail.com>
2021-06-20 12:15:14 -0700

    Getting the size of it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,5 +8,14 @@
 
 int main(void)
 {
-sizeof("");
+  int intType;
+  float floatType;
+  char charType;
+  double doubleType;
+
+  printf("The Size of int: %zu bytes\n", sizeof(intType));
+  printf("The size of float: %zu bytes\n", sizeof(floatType));
+  printf("The size of char: %zu bytes\n:", sizeof(charType));
+  printf("The size of double: %zu bytes\n:", sizeof(doubleType));
+  
 }
