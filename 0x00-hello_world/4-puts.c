5e24f60e5d5d3933a4560017f4588664023895d8
Frank6496 <francismuthoni254@gmail.com>
2021-06-20 09:51:51 -0700

    Text printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-//This is a single-line comment
-
+/**
+ *This is a C programm
+ *it is writing back a text
+*/
 int main(void)
 {
    puts( "Programming is like building a multilingual puzzle,\n" );
