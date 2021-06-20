81084b72c5ee80ad6769254068b310873f2ea85c
Frank6496 <francismuthoni254@gmail.com>
2021-06-20 10:36:07 -0700

    printing a text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void) /* This is the main func */
 {
-puts("\"Programming is like building a multilingual puzzle, 0\n");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
