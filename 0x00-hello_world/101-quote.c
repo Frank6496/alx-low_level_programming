5b81c819307db4561f9d24d3ea4f38b090ace90a
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 01:37:35 -0700

    Using the write func

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
  * main - entry point
  *
  * return: always 0 (succes)
+ *
  */
+
 int main(void)
 {
 fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
