0e80bc04204d12aa05fc3a40adb1250972d010a5
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 05:32:24 -0700

    Determing if a no is +ve, -ve or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 /**
- * main - print if the number is positive, zero or negative
- *
- * Description: using the main function
- *this program prints the out
- *Return: 0
- */
+* main - print if the number is positive, zero or negative
+*
+* Description: using the main function
+*this program prints the out
+*Return: 0
+*/
+
 int main(void)
 {
 int n;
@@ -16,15 +17,17 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
- 
-if(n == 0) {
-  printf("%i is zero\n", n);
+if (n == 0)
+{
+printf("%i is zero\n", n);
 }
-else if (n > 0) {
-  printf("%i is positive\n", n);
+else if (n > 0)
+{
+printf("%i is positive\n", n);
 }
-else if (n < 0) {
-  printf("%i is negative\n", n);
-}	
+else if (n < 0)
+{
+printf("%i is negative\n", n);
+}
 return (0);
 }
