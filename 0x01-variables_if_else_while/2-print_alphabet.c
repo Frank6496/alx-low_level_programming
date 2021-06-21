ff0169c063a332cdca1a28386431ccd47ee68453
Francis254 <59084931+Frank6496@users.noreply.github.com>
2021-06-22 00:11:07 +0300

    Create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - print alphabet
+* x: temp var
+* Description: lower alpha
+* Return: Always 0 succes
+*/
+int main(void)
+{
+int x;
+
+for (x = 'A'; x <= 'z'; x++)
+{
+x = tolower(x);
+putchar(x);
+}
+putchar('\n');
+return(0);
+}
