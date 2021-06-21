ca764c5db5f82a3863d271c0621e7de0d6c7840f
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 02:50:05 -0700

    using the sizeOf func

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ char charType;
 long longType;
 long long longyType;
 
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of char: %d byte(s)\n", sizeof(charType));
+printf("Size of a an int: %d byte(s)\n", sizeof(intType));
+printf("Size of a long int: %d byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longyType));
+printf("Size of a float: %d byte(s)\n", sizeof(floatType));
 
 return (0);
 }
