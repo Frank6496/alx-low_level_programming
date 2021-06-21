646a7b501f314b6ef822c7c1bb1879521aefb149
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 13:43:28 -0700

    Random no check

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,22 @@
 */
 int main(void)
 {
-int n;
+  int n, lastdigit;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
-if (n > 5)
+if (lastdigit > 5)
 {
-printf("%d and is greater than 5\n", n);
+printf("last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
-else if (n == 0)
+else if (lastdigit == 0)
 {
-printf("%d and is 0\n", n);
+printf("last digit of %d is %d and is 0\n", n, lastdigit);
 }
-else if ((n < 6) && (n != 0))
+else if (lastdigit < 6 && lastdigit != 0))
 {
-printf("%d and is less than 6 and not 0 \n", n);
+printf("Last digit %d is %d and is less than 6 and not 0 \n", n);
 }
 
 return (0);
