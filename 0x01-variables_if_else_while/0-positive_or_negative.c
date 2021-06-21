51423104a66a7d765f32efc107f1220f0c243d02
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 04:14:59 -0700

    Determining +ve or -ve no

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,16 +13,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 if(n == 0) {
-puts("is zero\n");
+printf("is zero\n");
 }
 else if (n > 0) {
-puts("is positive\n");
+printf("is positive\n");
 }
 else if (n < 0) {
-puts("is negative\n");
+printf("is negative\n");
 }
 else {
-puts("invalid\n");
+printf("invalid\n");
 }
 	
 return (0);
