fd8c4009a2f4844212012f1e809ba91431944154
Frank6496 <francismuthoni254@gmail.com>
2021-06-21 07:36:28 -0700

    print last digit if +ve or -ve

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,10 +5,10 @@
 /**
 * main - entry point
 *
-* Description: it returns greater than 5 if the last digit is greater than 5 
+* Description: it returns greater than 5 if the last digit is greater than 5
 * if its less than 5 it returns less than 5
 * if last digit of n < 6 && not 0 it returns less than 6 and not 0
-* return: always 0 (succes
+* return: always 0 (succes)
 */
 /* betty style doc for function main goes there */
 int main(void)
@@ -20,15 +20,15 @@ n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if (n > 5)
 {
-printf(n, "\n and is greater than 5");
+printf(n, "%d and is greater than 5\n");
 }
 else if (n == 0)
 {
-printf(n, "\n and is 0");
+printf(n, "%d and is 0\n");
 }
 else if ((n < 6) && (n == 0))
 {
-printf(n, "\and is less than 6 and not 0\n");
+printf(n, "%d and is less than 6 and not 0 \n");
 }
 
 return (0);
