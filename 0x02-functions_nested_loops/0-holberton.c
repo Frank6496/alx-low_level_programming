7859a4adf5bb675752d6b739b91df3272ef9cb95
Frank6496 <francismuthoni254@gmail.com>
2021-06-22 10:35:03 -0700

    Hello .h file holber

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,6 +9,12 @@
 */
 int main(void) /* The main func */
 {
-_putchar("Holberton\n");
+char c;
+char phrase[] = "Holberton";
+for (c = 0; c <= 9; c++)
+{
+_putchar(phrase[c]);
+_putchar("\n");
+}
 return (0);
 }
