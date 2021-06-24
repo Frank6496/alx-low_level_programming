528063464c5da16a1d1e23b74ef605f8ddbc2f9b
Frank6496 <francismuthoni254@gmail.com>
2021-06-24 18:48:25 +0300

    This func prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,28 +7,28 @@
  */
 void print_triangle(int size)
 {
-	int a, b;
+int a, b;
 
-	if (size > 0)
-	{
-		for (a = 0 ; a < size ; a++)
-		{
-			for (b = 0 ; b < size ; b++)
-			{
-				if (b < size - (a + 1))
-				{
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar(35);
-				}
-			}
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (size > 0)
+{
+for (a = 0 ; a < size ; a++)
+{
+for (b = 0 ; b < size ; b++)
+{
+if (b < size - (a + 1))
+{
+_putchar(' ');
+}
+else
+{
+_putchar(35);
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
